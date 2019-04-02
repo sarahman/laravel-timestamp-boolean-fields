732eff7e155945c68235209465605cd623b5664e
Syed Abidur Rahman <aabid048@gmail.com>
2019-04-02 22:50:21 +0600

    The test after updating the bool-timestamp field value is done.

--- tests/BooleanTimestampFieldManipulatorTest.php
+++ tests/BooleanTimestampFieldManipulatorTest.php
@@ -46,4 +46,36 @@ class BooleanTimestampFieldManipulatorTest extends PHPUnit_Framework_TestCase
         $this->assertFalse($user2->is_active);
         $this->assertNull($user2->time_being_active);
     }
+
+    /** @test */
+    public function it_checks_timestamp_boolean_field_value_when_updating_a_model()
+    {
+        $user = Tests\Entities\User::where('name', 'Syed Abidur Rahman')->first();
+        if (empty($user)) {
+            $user = Tests\Entities\User::create([
+                'name' => 'Syed Abidur Rahman',
+                'is_active' => 1
+            ]);
+        }
+
+        $user->update(['is_active' => 0]);
+
+        $this->assertNotNull($user->is_active);
+        $this->assertFalse($user->is_active);
+        $this->assertNull($user->time_being_active);
+
+        $user2 = Tests\Entities\User::where('name', 'Md Sadiqur Rahman')->first();
+        if (empty($user2)) {
+            $user2 = Tests\Entities\User::create([
+                'name' => 'Md Sadiqur Rahman',
+                'is_active' => 0
+            ]);
+        }
+
+        $user2->update(['is_active' => 1]);
+
+        $this->assertNotNull($user2->is_active);
+        $this->assertTrue($user2->is_active);
+        $this->assertInstanceOf(\Carbon\Carbon::class, $user2->time_being_active);
+    }
 }
