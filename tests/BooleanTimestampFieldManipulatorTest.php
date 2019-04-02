efce48b736b743e1a8da383a6b4bdbe8e570bc3c
Syed Abidur Rahman <aabid048@gmail.com>
2019-04-02 22:57:51 +0600

    The test user creation functionality is moved into the test class booting.

--- tests/BooleanTimestampFieldManipulatorTest.php
+++ tests/BooleanTimestampFieldManipulatorTest.php
@@ -4,6 +4,8 @@ use Illuminate\Database\Capsule\Manager;
 
 class BooleanTimestampFieldManipulatorTest extends PHPUnit_Framework_TestCase
 {
+    private static $user, $user2;
+
     public static function setupBeforeClass()
     {
         $manager = new Manager();
@@ -23,59 +25,41 @@ class BooleanTimestampFieldManipulatorTest extends PHPUnit_Framework_TestCase
             $table->timestamp('is_active')->nullable();
             $table->timestamps();
         });
+
+        self::$user = Tests\Entities\User::create([
+            'name' => 'Syed Abidur Rahman',
+            'is_active' => 1
+        ]);
+
+        self::$user2 = Tests\Entities\User::create([
+            'name' => 'Md Sadiqur Rahman',
+            'is_active' => 0
+        ]);
     }
 
     /** @test */
     public function it_checks_timestamp_boolean_field_value_when_creating_a_model()
     {
-        $user = Tests\Entities\User::create([
-            'name' => 'Syed Abidur Rahman',
-            'is_active' => 1
-        ]);
+        $this->assertNotNull(self::$user->is_active);
+        $this->assertTrue(self::$user->is_active);
+        $this->assertInstanceOf(\Carbon\Carbon::class, self::$user->time_being_active);
 
-        $this->assertNotNull($user->is_active);
-        $this->assertTrue($user->is_active);
-        $this->assertInstanceOf(\Carbon\Carbon::class, $user->time_being_active);
-
-        $user2 = Tests\Entities\User::create([
-            'name' => 'Md Sadiqur Rahman',
-            'is_active' => 0
-        ]);
-
-        $this->assertNotNull($user2->is_active);
-        $this->assertFalse($user2->is_active);
-        $this->assertNull($user2->time_being_active);
+        $this->assertNotNull(self::$user2->is_active);
+        $this->assertFalse(self::$user2->is_active);
+        $this->assertNull(self::$user2->time_being_active);
     }
 
     /** @test */
     public function it_checks_timestamp_boolean_field_value_when_updating_a_model()
     {
-        $user = Tests\Entities\User::where('name', 'Syed Abidur Rahman')->first();
-        if (empty($user)) {
-            $user = Tests\Entities\User::create([
-                'name' => 'Syed Abidur Rahman',
-                'is_active' => 1
-            ]);
-        }
+        self::$user->update(['is_active' => 0]);
+        $this->assertNotNull(self::$user->is_active);
+        $this->assertFalse(self::$user->is_active);
+        $this->assertNull(self::$user->time_being_active);
 
-        $user->update(['is_active' => 0]);
-
-        $this->assertNotNull($user->is_active);
-        $this->assertFalse($user->is_active);
-        $this->assertNull($user->time_being_active);
-
-        $user2 = Tests\Entities\User::where('name', 'Md Sadiqur Rahman')->first();
-        if (empty($user2)) {
-            $user2 = Tests\Entities\User::create([
-                'name' => 'Md Sadiqur Rahman',
-                'is_active' => 0
-            ]);
-        }
-
-        $user2->update(['is_active' => 1]);
-
-        $this->assertNotNull($user2->is_active);
-        $this->assertTrue($user2->is_active);
-        $this->assertInstanceOf(\Carbon\Carbon::class, $user2->time_being_active);
+        self::$user2->update(['is_active' => 1]);
+        $this->assertNotNull(self::$user2->is_active);
+        $this->assertTrue(self::$user2->is_active);
+        $this->assertInstanceOf(\Carbon\Carbon::class, self::$user2->time_being_active);
     }
 }
